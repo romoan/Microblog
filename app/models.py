f8118c8a2b7a7ce97764402d26e018c3d6b3309e
Robe Moreno <romoan@gmail.com>
2018-07-14 11:31:18 +0200

    Add login process

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db
+from app import db, login
 from flask_login import UserMixin
 
 
@@ -29,3 +29,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
